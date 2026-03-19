b750ec4b5908195567ef8a267efa872edfcfd158
Niels-de-Win <r1092919@student.thomasmore.be>
2026-03-19 09:14:14 +0100

    fixed temprary python

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def blink(led_obj, number_blinks, period, duty_cycle):
         time_high = period * (duty_cycle / 100)
         time_low = period - time_high
         for i in range(number_blinks):
-        led_obj.value = True
-        time.sleep(time_high)
-        led_obj.value = False
-        time.sleep(time_low)
+            led_obj.value = True
+            time.sleep(time_high)
+            led_obj.value = False
+            time.sleep(time_low)
 
 # Blink LED 20 times
 blink(led, 20, 0.5, 75)
